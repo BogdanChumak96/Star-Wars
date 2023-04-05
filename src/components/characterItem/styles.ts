2e1f1997c3cf2f80a203066b9b27b853e87bc2bc
BogdanChumak96 <Bogdanghost0@gmail.com>
2023-04-05 05:42:12 +0300

    change size item

--- src/components/characterItem/styles.ts
+++ src/components/characterItem/styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native'
 export const styles = StyleSheet.create({
   container: {
     marginVertical: 8,
-    width: '88%',
+    width: '91%',
     color: 'white',
     backgroundColor: 'rgba(0,0,0,0.2)',
     borderRadius: 10,
